987db03f0b992830c7818b88ab0f15fd3b6b4929
Joary13 <jolea13008@gmail.com>
2023-11-29 16:01:15 +0100

    chidren props used again

--- src/App.js
+++ src/App.js
@@ -40,22 +40,15 @@ function Steps() {
             <div className={step >= 2 ? 'active' : ''}>2</div>
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
-          <p className='message'>
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className='buttons'>
-            <Button
-              textColor='#fff'
-              bgColor='#7950f2'
-              onClick={handlePrevious}
-              content='previous'
-            />
-            <Button
-              textColor='#fff'
-              bgColor='#7950f2'
-              onClick={handleNext}
-              content='next'
-            />
+            <Button textColor='#fff' bgColor='#7950f2' onClick={handlePrevious}>
+              <span>👈</span>
+              previous
+            </Button>
+            <Button textColor='#fff' bgColor='#7950f2' onClick={handleNext}>
+              next<span>👉</span>
+            </Button>
           </div>
         </div>
       )}
@@ -63,13 +56,22 @@ function Steps() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, content }) {
+function StepMessage({ step, children }) {
+  return (
+    <div className='message'>
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
       style={{ backgroundColor: `${bgColor}`, color: `${textColor}` }}
       onClick={onClick}
     >
-      {content}
+      {children}
     </button>
   );
 }
